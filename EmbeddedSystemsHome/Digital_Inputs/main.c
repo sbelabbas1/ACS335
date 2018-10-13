2ed31e9b1138e1b08c6ea10be4753b96599ad8b6
sbelabbas1 <sbelabbas1@sheffield.ac.uk>
2018-10-13 17:52:44 +0100

    DigitalInputs: Exercise two completed

--- EmbeddedSystemsHome/Digital_Inputs/main.c
+++ EmbeddedSystemsHome/Digital_Inputs/main.c
@@ -1,8 +1,19 @@
-#include "stm32f4xx.h"
+#include "stm32f407xx.h"
 
 int main(void){
-	//Initiate GPIOD and GREEN LED
-	RCC->AHB1ENR |=	0X000008;
-	GPIOD->MODER |= GPIO_MODER_MODER12_0; 	
 	
+	//Initiate GPIOD and GREEN LED
+	RCC->AHB1ENR |=	RCC_AHB1ENR_GPIODEN; 	//Initiate clock for GPIOD Green LED
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; 	//Initiate clock for GPIOD Green LED 
+	GPIOD->MODER |= GPIO_MODER_MODE12_0; 	//Initiats the GPIOD Green LED into general purpose output mode
+	//GPIOA->MODER &= ~GPIO_MODER_MODER0;		//Initiate clock for GPIOA
+	GPIOD->MODER &= ~0X00000003; // Resets moder 0 to input mode 
+	for(;;){
+		if(((GPIOA->IDR) & 0X0001) == 0X0001){ // Read for button if it is pressed
+			GPIOD->BSRR = 0x00001000;	//Activate Green LED
+		}
+		else{
+			GPIOD->BSRR = 0x10000000;	//Disable Green LED 
+		}
+	}
 };
